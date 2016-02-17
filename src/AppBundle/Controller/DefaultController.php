9bc399a76502069886a87358fc2e2ee245df7152
Quentin Delance <quentin.delance@gmail.com>
2016-02-17 19:00:07 +0100

    Edit/delete mostly OK

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,11 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Partner;
+use AppBundle\Form\PartnerType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
@@ -71,38 +74,83 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/partners/{id}/delete", name="partner_delete")
+     * @Route("/partner/{id}/delete", name="partner_delete")
      */
     public function partnerDeleteAction(Request $request, $id)
     {
-        $repository = $this
-            ->getDoctrine()
-            ->getRepository('AppBundle:Partner');
+        $em = $this->getDoctrine()->getManager();
+        $partner = $em->getRepository('AppBundle:Partner')->find($id);
+        if (null === $partner) {
+            throw new NotFoundHttpException('No partner for id ' . $id);
+        }
+        $form = $this->createFormBuilder()->getForm();
+        if ($form->handleRequest($request)->isValid()) {
+            $em->remove($partner);
+            $em->flush();
+            $this->addFlash('info', 'Partner deleted');
 
-        $partner = $repository->find($id);
-
-        if ($partner === null) {
-            throw $this->createNotFoundException('No partner found for id ' . $id);
+            return $this->redirect($this->generateUrl('partner_list'));
         }
 
-        return $this->render('default/partner.html.twig', array('partner' => $partner));
+        // confirm page
+        return $this->render('default/partner_delete.html.twig', array('form' => $form->createView(), 'partner' => $partner));
     }
 
     /**
-     * @Route("/partners/{id}/edit", name="partner_edit")
+     * @Route("/partner/{id}/edit", name="partner_edit")
      */
     public function partnerEditAction(Request $request, $id)
     {
-        $repository = $this
-            ->getDoctrine()
-            ->getRepository('AppBundle:Partner');
+        $em = $this->getDoctrine()->getManager();
+        $partner = $em
+            ->getRepository('AppBundle:Partner')
+            ->find($id);
 
-        $partner = $repository->find($id);
-
-        if ($partner === null) {
-            throw $this->createNotFoundException('No partner found for id ' . $id);
+        if (null === $partner) {
+            throw new NotFoundHttpException('No partner for id ' . $id);
         }
 
-        return $this->render('default/partner.html.twig', array('partner' => $partner));
+        $form = $this->createForm(PartnerType::class, $partner);
+        // $form = $this->get('form.factory')->create(new AdvertType, $advert);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($partner);
+            $em->flush();
+
+            $this->addFlash('info', 'Partner saved');
+
+            return $this->redirect($this->generateUrl('partner_view', array('id' => $partner->getId())));
+        }
+
+        return $this->render('default/partner_edit.html.twig', array('form' => $form->createView(), 'partner' => $partner));
+    }
+
+    /**
+     * @Route("/partner/add", name="partner_add")
+     */
+    public function partnerAddAction(Request $request)
+    {
+        $partner = new Partner();
+
+        $form = $this->createForm(PartnerType::class, $partner);
+        // $form = $this->get('form.factory')->create(new AdvertType(), $advert);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($partner);
+            $em->flush();
+
+            $this->addFlash('info', 'Partner added');
+
+            return $this->redirect($this->generateUrl('partner_view', array('id' => $partner->getId())));
+        }
+
+        return $this->render('default/partner_edit.html.twig', array('form' => $form->createView()));
+
     }
 }
